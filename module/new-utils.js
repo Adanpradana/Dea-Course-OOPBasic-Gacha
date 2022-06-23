edb7109988d12b61d00abbb36619eec55d7491be
Adanpradana <adanizkahariz@gmail.com>
2022-06-23 11:09:17 +0700

    bug fix start button

--- module/new-utils.js
+++ module/new-utils.js
@@ -99,7 +99,7 @@ const display = () => {
   navbar.style.justifyContent = "space-between";
   logout.style.display = "block";
   goToTop.style.display = "flex";
-  startSection.style.display = "flex";
+  startSection.style.display = "block";
   clearGame();
   // level 1 gacha box
   getBoxLevel1();
